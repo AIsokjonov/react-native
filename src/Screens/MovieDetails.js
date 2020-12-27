1ff53915cb3844222f6f96d2d7867a63b50ae705
AIsokjonov <abdurauf.isokjonov@gmail.com>
2020-12-27 17:30:11 +0900

    movie details scrollview

--- src/Screens/MovieDetails.js
+++ src/Screens/MovieDetails.js
@@ -6,24 +6,27 @@ import {
 	View,
 	Text,
 	Button,
-	Image
+	Image,
+	ScrollView
 } from 'react-native';
 
 const MovieDetails = ({ navigation, route }) => {
 	const { movie } = route.params;
 	return (
 		<SafeAreaView style={styles.container}>
-			<Image style={styles.img} source={{uri: movie.image}} />
-			<Text style={styles.title}>{movie.title}</Text>
-			<Text style={styles.overview}>{movie.overview}</Text>
-			<Text>
-				<View><Text style={styles.rating}>Rating: {movie.vote_average}</Text></View>
-				<View><Text style={styles.release_date}>Release Date: {movie.release_date}</Text></View>
-			</Text>
-			<Button
-				title="Home" 
-				onPress={() => navigation.navigate('Home')} 
-			/>
+			<ScrollView>
+				<Image style={styles.img} source={{ uri: movie.image }} />
+				<Text style={styles.title}>{movie.title}</Text>
+				<Text style={styles.overview}>{movie.overview}</Text>
+				<Text>
+					<View><Text style={styles.rating}>Rating: {movie.vote_average}</Text></View>
+					<View><Text style={styles.release_date}>Release Date: {movie.release_date}</Text></View>
+				</Text>
+				<Button
+					title="Home"
+					onPress={() => navigation.navigate('Home')}
+				/>
+			</ScrollView>
 		</SafeAreaView>
 	);
 };
