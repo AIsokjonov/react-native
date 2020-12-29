e5e282b1c7315697329ff23a996a6f94832677f6
AIsokjonov <abdurauf.isokjonov@gmail.com>
2020-12-29 18:25:21 +0900

    infinite scrolling
    infinite scrolling with flatlist

--- src/Screens/Browse.js
+++ src/Screens/Browse.js
@@ -1,5 +1,5 @@
 import React, { useState, useEffect } from 'react';
-import { StyleSheet, View, Text, FlatList, ImagePropTypes } from 'react-native';
+import { StyleSheet, View, Text, FlatList, TouchableOpacity, SafeAreaView } from 'react-native';
 import axios from 'axios';
 
 import Movie from '../components/Movie';
@@ -19,71 +19,64 @@ const Browse = (props) => {
   const [loading, setLoading] = useState(false);
   const [movies, setMovies] = useState([]);
   const [error, setError] = useState('');
+  const [offset, setOffset] = useState(1);
 
   useEffect(() => {
-    function fetchPopular() {
-      setLoading(true);
-      try {
-        axios.get('https://api.themoviedb.org/3/movie/popular?api_key=a1279933de606b4374a2c93a1d0127a9&language=en-US&page=1')
-          .then((res) => {
-            const moviesLoaded = res.data.results.map((movie) => ({
-              ...movie,
-              image: getImage(movie.poster_path),
-            }))
+    fetchMovies()
+  }, []);
 
-            setMovies(moviesLoaded);
-          })
-      } catch (error) {
-        setError('Something went wrong');
-      } finally {
-        setLoading(false);
-      }
+  async function fetchMovies() {
+    setLoading(true);
+    try {
+      await axios.get(`https://api.themoviedb.org/3/movie/popular?api_key=a1279933de606b4374a2c93a1d0127a9&language=en-US&page=${offset}`)
+        .then((res) => {
+          const loadedMovies = res.data.results.map((movie) => ({
+            ...movie,
+            image: getImage(movie.poster_path),
+          }))
+
+          setMovies(loadedMovies);
+          setOffset(offset + 1);
+        })
+    } catch (error) {
+      setError('Something went wrong');
+    } finally {
+      setLoading(false);
     }
-
-    fetchPopular();
-  })
+  }
 
   return (
-    <View style={styles.container}>
+    <SafeAreaView style={styles.container}>
       <View style={styles.popular}>
-        <Text style={styles.head}>Browse Page</Text>
-        <View>
-          <FlatList
-            contentContainerStyle={styles.container}
-            data={movies}
-            horizontal={false}
-            numColumns={3}
-            renderItem={({ item }) => (
-              <Movie item={item} navigation={props.navigation} />
-            )}
-            keyExtractor={(item) => item.id.toString()}
-          />
-        </View>
+        <FlatList
+          contentContainerStyle={styles.container}
+          data={movies}
+          horizontal={false}
+          numColumns={3}
+          renderItem={({ item }) => (
+            <Movie item={item} navigation={props.navigation} />
+          )}
+          keyExtractor={(item) => item.id.toString()}
+          onEndReached={fetchMovies}
+          onEndReachedThreshold={0}
+        />
       </View>
-    </View>
+    </SafeAreaView>
   );
 };
 
 const styles = StyleSheet.create({
   container: {
-    justifyContent: 'center',
-    width: 380,
+    width: 375,
     alignItems: 'center',
+    marginBottom: 10,
   },
   list: {
-    flex: 1,
     justifyContent: 'center',
     alignItems: 'center'
   },
   popular: {
     marginTop: 10,
-  },
-  head: {
-    fontSize: 20,
-    fontWeight: 'bold',
-    textAlign: 'center',
-    marginTop: 30,
-    marginBottom: 10,
   }
 });
 
--- src/components/Movie.js
+++ src/components/Movie.js
@@ -23,7 +23,7 @@ const styles = StyleSheet.create({
 		
 	},
 	image: {
-		width: 117,
+		width: 120,
 		height: 148,
 	}
 });
